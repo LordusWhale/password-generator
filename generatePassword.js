dfd1f6eeaada3bdedfbf69e9e0b2da13cf44d856
Sam Sweeney <samsweeneyy@gmail.com>
2022-11-17 17:48:32 +1100

    Commit

---
+++ generatePassword.js
@@ -0,0 +1,54 @@
+const letters = "abcdefghijklmnopqrstuvwxyz".split("");
+const numbers = "123456789".split("");
+const special = "!#$%&()*+,-./:;<=>?@[}^_`{}~".split("");
+const CheckPassContains = (characters, password) => {
+  let num = 0;
+  characters.forEach((character) => {
+    if (password.includes(character)) num += 1;
+  });
+
+  return num > 0 ? true : false;
+};
+export const generatePassword = (
+  numLength,
+  includeSpecial,
+  includeUpper,
+  includeLower,
+  includeNumber
+) => {
+  let upperCase = letters.map((letter) => {
+    return letter.toUpperCase();
+  });
+  let characters = [];
+  if (includeSpecial) characters = characters.concat(special);
+  if (includeUpper) characters = characters.concat(upperCase);
+  if (includeLower) characters = characters.concat(letters);
+  if (includeNumber) characters = characters.concat(numbers);
+
+  let password = new Array(numLength).fill("").map((character) => {
+    return characters[Math.floor(Math.random() * characters.length)];
+  });
+  let isValid = true;
+  if (includeSpecial) {
+    isValid = CheckPassContains(special, password);
+  }
+  if (includeUpper) {
+    isValid = CheckPassContains(upperCase, password);
+  }
+  if (includeLower) {
+    isValid = CheckPassContains(letters, password);
+  }
+  if (includeNumber) {
+    isValid = CheckPassContains(numbers, password);
+  }
+  if (!isValid)
+    return generatePassword(
+      numLength,
+      includeSpecial,
+      includeUpper,
+      includeLower,
+      includeNumber
+    );
+
+  return password.join("");
+};
--- script.js
+++ script.js
@@ -1,17 +1,56 @@
-// Assignment code here
+import { generatePassword } from "./generatePassword.js";
+
+const numLengthInput = document.getElementById("number");
+const numLengthSlider = document.getElementById("slider");
+const passwordShow = document.getElementById("password");
+const specialCheck = document.getElementById("special");
+const upperCheck = document.getElementById("uppercase");
+const lowerCheck = document.getElementById("lowercase");
+const numberCheck = document.getElementById("numbers");
 
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let num = parseInt(numLengthInput.value);
+  if (num < 8 || num > 128) {
+    
+    passwordShow.innerHTML = "";
+  } else {
+    if (numLengthInput.value === "") {
+      passwordShow.innerHTML = "";
+      return;
+    }
+    let password = generatePassword(
+      num,
+      specialCheck.checked,
+      upperCheck.checked,
+      lowerCheck.checked,
+      numberCheck.checked
+    );
+    passwordShow.innerText = password;
+  }
+}
+numLengthInput.addEventListener("input", (e) => {
+  numLengthSlider.value = e.target.value;
+  writePassword();
 
-  passwordText.value = password;
+});
+numLengthSlider.addEventListener("input", (e)=>{
+  numLengthInput.value = e.target.value
+  writePassword();
+
+})
+specialCheck.addEventListener("click", writePassword);
+upperCheck.addEventListener("click", writePassword);
+lowerCheck.addEventListener("click", writePassword);
+numberCheck.addEventListener("click", writePassword);
+
+
+var generateBtn = document.querySelector("#generate");
+function showPasswordOptions() {
+  const form = document.querySelector('form');
+  generateBtn.style.visibility = 'hidden';
+  form.style.visibility = "visible"
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", showPasswordOptions);
